a625e89f1afd26a7bab4146fa1303f078cfc6f89
nahid_dev <nahid.dev19@gmail.com>
2024-02-14 20:52:03 +0600

    refactoring news component

--- src/components/LeftNewsComponent.jsx
+++ src/components/LeftNewsComponent.jsx
@@ -4,48 +4,60 @@ import thumb from "/src/assets/thumb.png";
 import thumb_lg from "/src/assets/thumb_lg.png";
 
 export default function LeftNewsComponent({ item, index }) {
-  return (
-    <div
-      className={`${
-        index == 0
-          ? "col-span-12 grid grid-cols-12 gap-4"
-          : index == 1
-          ? "col-span-12 grid grid-cols-12 gap-4 lg:col-span-8"
-          : "col-span-12 md:col-span-6 lg:col-span-4"
-      }`}
-    >
-      {/* info */}
-      <div
-        className={`${
-          index == 0
-            ? "col-span-12 lg:col-span-4"
-            : index == 1
-            ? "col-span-12 md:col-span-6"
-            : "col-span-12 md:col-span-4"
-        }`}
-      >
-        <a href="">
-          <h3 className="mb-2.5 text-xl font-bold lg:text-2xl">{item.title}</h3>
-        </a>
-        <p className="text-base text-[#292219]">{item.description}</p>
-        <p className="mt-5 text-base text-[#5C5955]">
-          {convertDateFormat(item.publishedAt)}
-        </p>
-      </div>
-      {/* thumb */}
-      {index == 0 ? (
+  const infoClasses = [
+    "mb-2.5 text-xl font-bold",
+    index === 0 ? "lg:text-2xl" : null,
+  ];
+
+  const renderThumbnail = () => {
+    if (index === 0) {
+      return (
         <div className="col-span-12 lg:col-span-8">
-          <img className="w-full" src={thumb_lg} alt="thumb" />
-          <p class="mt-5 text-base text-[#5C5955]">
+          <img className={"w-full"} src={thumb_lg} alt="thumb" />
+          <p className="mt-5 text-base text-[#5C5955]">
             Illustration: Karolis Strautniekas
           </p>
         </div>
-      ) : index == 1 ? (
+      );
+    } else if (index === 1) {
+      return (
         <div className="col-span-12 md:col-span-6">
-          <img className="w-full" src={thumb} alt="thumb" />
+          <img className={"w-full"} src={thumb} alt="thumb" />
         </div>
-      ) : null}
-      {/* */}
+      );
+    } else {
+      return null;
+    }
+  };
+
+  return (
+    <div
+      className={`${
+        index === 0
+          ? " grid grid-cols-12 gap-4"
+          : index === 1
+          ? " grid grid-cols-12 gap-4 lg:col-span-8"
+          : " md:col-span-6 lg:col-span-4"
+      } col-span-12`}
+    >
+      <div
+        className={`${
+          index === 0
+            ? "lg:col-span-4"
+            : index === 1
+            ? " md:col-span-6"
+            : " md:col-span-4"
+        } col-span-12 `}
+      >
+        <a href="">
+          <h3 className={infoClasses.join(" ")}>{item.title}</h3>
+        </a>
+        <p className={"text-base text-[#292219]"}>{item.description}</p>
+        <p className={"mt-5 text-base text-[#5C5955]"}>
+          {convertDateFormat(item.publishedAt)}
+        </p>
+      </div>
+      {renderThumbnail()}
     </div>
   );
 }
--- src/components/RightNewsComponent.jsx
+++ src/components/RightNewsComponent.jsx
@@ -4,14 +4,14 @@ import thumb from "/src/assets/thumb.png";
 
 export default function RightNewsComponent({ item, index }) {
   return (
-    <div class={`col-span-12 md:col-span-8 ${index == 0 ? "mb-6" : ""}`}>
+    <div className={`col-span-12 md:col-span-8 ${index == 0 ? "mb-6" : ""}`}>
       {index == 0 && <img className="w-full" src={thumb} alt="thumb" />}
-      <div class={`col-span-12 md:col-span-4 ${index == 0 ? "mt-6" : "mt-2"}`}>
+      <div className={`col-span-12 md:col-span-4 ${index == 0 ? "mt-6" : "mt-2"}`}>
         <a href="#">
-          <h3 class="mb-2.5 text-xl font-bold lg:text-[20px]">{item?.title}</h3>
+          <h3 className="mb-2.5 text-xl font-bold lg:text-[20px]">{item?.title}</h3>
         </a>
-        <p class="text-base text-[#292219]">{item?.description}</p>
-        <p class="mt-5 text-base text-[#94908C]">
+        <p className="text-base text-[#292219]">{item?.description}</p>
+        <p className="mt-5 text-base text-[#94908C]">
           {" "}
           {convertDateFormat(item?.publishedAt)}
         </p>
